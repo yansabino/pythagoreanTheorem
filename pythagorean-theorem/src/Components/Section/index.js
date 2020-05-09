ee99de01d6e805ca0c56e1850be21e59746b1cf1
Yan Sabino <yan.sabino@hotmail.com>
2020-05-08 21:08:18 -0300

    Thinking on a way to resolve the requests bugs

--- pythagorean-theorem/src/Components/Section/index.js
+++ pythagorean-theorem/src/Components/Section/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import {
   SectionContainer,
@@ -8,7 +8,7 @@ import {
 import Triangle from "../../images/kisspng-right-triangle-hypotenuse-mathematics-trigonometry-5b39d436497ff0.4595208215305165343011.png";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { useInputChange } from '../../hooks/useInputChange'
+
 
 const inputForm = [
   {
@@ -28,23 +28,35 @@ const inputForm = [
 const url = "https://atlas-231814.appspot.com/calcula";
 
 const Section = () => {
-  const [form, handleInputChange] = useInputChange()
+  const [form, setForm] = useState({});
+  const [response, setResponse] = useState({})
 
-  const calculate = () => {
-    const inputData = {
-      catetos: form.name,
-    };
-    console.log(inputData)
-    const request = axios.post(url, inputData);
+  const handleInputChange = (e) =>
+  setForm({
+    ...form,
+    [e.target.name]: e.target.value,
+  });
 
+  const calculate = (e) => {
+    e.preventDefault()
+
+    const request = axios.post(url, form);
+    
+    
     request
-      .then((response) => {
+      .then(() => {
+        setResponse(response)
         window.alert("Dados inputados com sucesso");
+        
+        console.log(response)
+        
       })
       .catch((err) => {
         console.log(err);
         window.alert("Houve um erro na hora de enviar os dados");
       });
+
+      setForm({})
   };
 
   const formRender = inputForm.map((input) => (
